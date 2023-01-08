cc5a2d8f05548515a2155b5f40aa4436b3d05cc3
Khalyavin <sts2004@gmail.com>
2023-01-08 13:24:27 +0700

    Queue ready

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -11,12 +11,84 @@ class Queue:
 
     def enqueue(self, data):
         """Добавить данные в очередь"""
-        pass
+        if self.tail is None and self.head is None:
+            self.head = self.tail = Node(data, None)
+            return
+        self.tail.next_node = Node(data, None)
+        self.tail = self.tail.next_node
 
     def dequeue(self):
         """Забрать данные из очереди"""
-        pass
+        if self.head is None:
+            return None
+
+        deleted = self.head.data
+        self.head = self.head.next_node
+        if self.head is None:
+            self.tail = None
+
+        return deleted
 
     def to_list(self):
         """Вернуть данные очереди в виде списка"""
-        pass
+        data_list = []
+
+        if self.head is None:
+            return data_list
+
+        node = self.head
+        while node:
+            data_list.append(node.data)
+            node = node.next_node
+
+        return data_list
+
+
+if __name__ == '__main__':
+    q = Queue()
+    for i in range(1, 11):
+        q.enqueue(i)
+        print(q.to_list())
+
+    for i in range(1, 12):
+        q.dequeue()
+        print(q.to_list())
+
+
+#  def test_queue_to_list():
+    q = Queue()
+
+    assert q.to_list() == []
+
+
+#  def test_queue_enqueue():
+    q = Queue()
+    q.enqueue('data1')
+    q.enqueue('data2')
+
+    assert q.to_list() == ['data1', 'data2']
+
+
+#  def test_queue_dequeue_empty():
+    q = Queue()
+    q.dequeue()
+
+    assert q.to_list() == []
+
+
+#  def test_queue_dequeue():
+    q = Queue()
+    q.enqueue('data1')
+    q.dequeue()
+
+    assert q.to_list() == []
+
+
+#  def test_queue_dequeue2():
+    q = Queue()
+    q.enqueue('data1')
+    q.enqueue('data2')
+    q.dequeue()
+
+    assert q.to_list() == ['data2']
+
