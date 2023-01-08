717f9f93b8feebc4f8780c53a4b1456a6caa9b4c
Khalyavin <sts2004@gmail.com>
2023-01-08 12:56:25 +0700

    Stack ready

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -48,12 +48,11 @@ if __name__ == '__main__':
         s.pop()
         print(s.to_list())
 
-#    def test_to_list():
+        #    def test_to_list():
         s = Stack()
         assert s.to_list() == []
 
-
-#    def test_push():
+        #    def test_push():
         s = Stack()
         s.push('data1')
         s.push({'key': 'value'})
@@ -61,15 +60,13 @@ if __name__ == '__main__':
 
         assert s.to_list() == [1000, {'key': 'value'}, 'data1']
 
-
-#    def test_pop_empty():
+        #    def test_pop_empty():
         s = Stack()
         removed = s.pop()
 
         assert removed is None
 
-
-#    def test_push_pop():
+        #    def test_push_pop():
         s = Stack()
         s.push(100)
         removed = s.pop()
@@ -77,8 +74,7 @@ if __name__ == '__main__':
         assert removed == 100
         assert s.to_list() == []
 
-
-#    def test_push_push_pop():
+        #    def test_push_push_pop():
         s = Stack()
         s.push(100)
         s.push([5])
@@ -87,4 +83,3 @@ if __name__ == '__main__':
         assert s.pop() == 100
         assert s.pop() is None
         assert s.to_list() == []
-
